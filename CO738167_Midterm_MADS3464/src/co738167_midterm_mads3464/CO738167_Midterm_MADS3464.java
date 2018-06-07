28d8551d4baea1c4bdc957376bc75fb99f0621cc
MacStudent <macstudent@20X-iMacXX-M-458.local>
2018-06-07 14:35:48 -0400

    First commit

---
+++ CO738167_Midterm_MADS3464/src/co738167_midterm_mads3464/CO738167_Midterm_MADS3464.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co738167_midterm_mads3464;
+
+/**
+ *
+ * @author macstudent
+ */
+public class CO738167_Midterm_MADS3464 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
